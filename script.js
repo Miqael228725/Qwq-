3d8a351a3181c1215b379aebdb7e0d61ee7107e9
MiqaGames <miqael2507@gmail.com>
2025-01-10 21:34:32 +0300

    create game field

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let info = document.querySelector('.info')
 let btn_start = document.querySelector('.btn')
 let main = document.querySelector('.main')
+let field = document.querySelector('.field')
 
 btn_start.addEventListener('click', function(){
     main.style.display = 'flex'
@@ -16,3 +17,42 @@ btn_start.addEventListener('click', function(){
     })
 
 })
+
+let rows = 20
+let colums = 10
+let cell_size = 30
+let game_board = []
+for(let i=0; i < rows; i+=1){
+    game_board.push(new Array(colums).fill(0))
+}
+function create_field(){
+    field.style.display = 'grid'
+    field.style.gridTemplateRows = `repeat(${rows}, ${cell_size}px)`
+    field.style.gridTemplateColums = `repeat(${colums}, ${cell_size}px)`
+    field.style.width = `${colums*cell_size}px`
+    field.style.height = `${rows*cell_size}px`
+    for(let i=0; i < rows * colums; i += 1){
+        let cell = document.createElement('div')
+        cell.style.border = '2px, solid, white'
+        cell.style.boxSizing = 'border-box'
+        field.appendChild(cell)
+    }
+}
+
+create_field()
+
+
+let cur_fig = {
+    shape: [
+        [1,1,1]
+        [0,0,1]
+    ],
+    x: 3,
+    y: 0
+}
+
+
+
+
+
+
