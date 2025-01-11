150a6c75842e9c51753bfe613d4d3868babcb2fb
MiqaGames <miqael2507@gmail.com>
2025-01-11 12:46:30 +0300

    add shapes

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ btn_start.addEventListener('click', function(){
 
 })
 
-let rows = 20
-let colums = 10
+let rows = 30
+let colums = 20
 let cell_size = 30
 let game_board = []
 for(let i=0; i < rows; i+=1){
@@ -28,12 +28,12 @@ for(let i=0; i < rows; i+=1){
 function create_field(){
     field.style.display = 'grid'
     field.style.gridTemplateRows = `repeat(${rows}, ${cell_size}px)`
-    field.style.gridTemplateColums = `repeat(${colums}, ${cell_size}px)`
+    field.style.gridTemplateColumns = `repeat(${colums}, ${cell_size}px)`
     field.style.width = `${colums*cell_size}px`
     field.style.height = `${rows*cell_size}px`
     for(let i=0; i < rows * colums; i += 1){
         let cell = document.createElement('div')
-        cell.style.border = '2px, solid, white'
+        cell.style.border = '1px solid darkgrey'
         cell.style.boxSizing = 'border-box'
         field.appendChild(cell)
     }
@@ -42,14 +42,83 @@ function create_field(){
 create_field()
 
 
-let cur_fig = {
-    shape: [
-        [1,1,1]
-        [0,0,1]
-    ],
-    x: 3,
-    y: 0
+// let cur_fig = {
+//     null
+// }
+
+let all_fig = [
+    {
+        shape: [
+            [1,1,1,1]
+        ],
+        color: 'purple'
+    },
+    {
+        shape: [
+            [1,1,1],
+            [0,0,1]
+        ],
+        color: 'blue'
+    },
+    {
+        shape: [
+            [0,1,0],
+            [1,1,1]
+        ],
+        color: 'red'
+    },
+    {
+        shape: [
+            [0,0,1],
+            [1,1,1]
+        ],
+        color: 'pink'
+    },
+    {
+        shape: [
+            [1,1],
+            [1,1]
+        ],
+        color: 'magenta'
+    },
+    {
+        shape: [
+            [0,1,1],
+            [1,1,0]
+        ],
+        color: 'cyan'
+    },
+    {
+        shape: [
+            [1,1,0],
+            [0,1,1]
+        ],
+        color: 'orange'
+    }
+]
+
+let cur_fig = null
+
+function draw_fig(obj, val=1){
+    obj.shape.forEach((row, i) => {
+        row.forEach((cell, j) => {
+            if (cell){
+                let x = obj.x+j
+                let y = obj.y+i
+                if (y >= 0 && y <= rows && x >= 0 && x <= colums-1){
+                    let index = y*colums+x
+                    let element_cell = field.children[index]
+                    element_cell.style.backgroundColor = val ? obj.color : ''
+                } 
+            }
+        });
+    });
 }
+
+
+
+
+
 
 
 
